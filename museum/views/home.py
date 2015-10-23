37ecc8fd4a1d0d6df713be3d2cde7480979c6088
whypro <whypro@live.cn>
2015-10-23 11:06:46 +0800

    Add pages.

--- museum/views/home.py
+++ museum/views/home.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import os
+from bson.objectid import ObjectId
 
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for, current_app, send_from_directory
+from flask import Blueprint, render_template, redirect, url_for, abort
 
 from ..extensions import mongo
 
@@ -11,11 +12,19 @@ home = Blueprint('home', __name__)
 
 @home.route('/')
 def index():
+    return redirect(url_for('home.show_masterpieces'))
+
+
+@home.route('/masterpiece/')
+def show_masterpieces():
     masterpieces = mongo.db.masterpieces.find().sort([('_id', -1)])
     print masterpieces.count()
-    return render_template('index.html', masterpieces=masterpieces)
+    return render_template('masterpieces.html', masterpieces=masterpieces)
 
 
-@home.route('/masterpiece/<filename>')
-def send_masterpiece(filename):
-    return send_from_directory(current_app.config['MASTERPIECES_DIR'], filename)
+@home.route('/masterpiece/<oid>/')
+def show_masterpiece_detail(oid):
+    masterpiece = mongo.db.masterpieces.find_one({'_id': ObjectId(oid)})
+    if not masterpiece:
+        abort(404)
+    return render_template('masterpiece_detail.html', masterpiece=masterpiece)
